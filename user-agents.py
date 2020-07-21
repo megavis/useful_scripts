3c05f7604215c72a020a3f9de47c36e23ec59c71
megavis <58000641+megavis@users.noreply.github.com>
2020-07-21 09:11:56 +0300

    Create user-agents.py

---
+++ user-agents.py
@@ -0,0 +1,18 @@
+    general_header_1 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                                     'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    general_header_2 = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) '
+                                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
+    general_header_3 = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:27.3) Gecko/20130101 Firefox/27.3'}
+
+
+    mobile_header_1 = {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) '
+                                     'AppleWebkit/534.30 '
+                                     '(KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'}
+
+    mobile_header_2 ={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) '
+                                    'AppleWebKit/536.26 (KHTML, like Gecko)'
+                                    ' Version/6.0 Mobile/10B350 Safari/8536.25'}
+
+    headers = [general_header_1, general_header_2, general_header_3, mobile_header_1,mobile_header_2]
